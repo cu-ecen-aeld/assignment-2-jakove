f90902cc4d5c985337bcafb1e5d0717f50853329
jakove <vonessenjakob@hotmail.de>
2023-10-29 11:13:11 +0100

    fix: handle io errors and chnage access rights

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -3,8 +3,6 @@
 #include <fcntl.h>
 #include <string.h>
 
-#include <stdio.h>
-
 int main(int argc, char* argv[]) {
     if(argc < 3) {
         syslog(LOG_ERR, "Invalid number of arguments! Please provide: filename and string");
@@ -14,7 +12,13 @@ int main(int argc, char* argv[]) {
         syslog(LOG_WARNING, "Invalid number of arguments! Provide only filename and string. Ignoring all other arguments");
     }
     
-    int fd = open(argv[1], O_RDWR | O_CREAT, S_IRUSR | S_IRGRP | S_IROTH);
+    int fd = open(argv[1], O_RDWR|O_CREAT, 0666);
+    if(fd == -1) {
+        syslog(LOG_ERR, "Could not open file: %s", argv[1]);
+        return 1;
+    }
+
+    syslog(LOG_DEBUG, "Writing %s to %s", argv[2], argv[1]);
 
     write(fd, argv[2], strlen(argv[2]));
     close(fd);
